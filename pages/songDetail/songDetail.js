b713e93bfe5043cbe4e3bf0cd19b1e2e8def85fb
柚子 <1442401757@qq.com>
2022-03-13 10:36:22 +0800

    master

--- pages/songDetail/songDetail.js
+++ pages/songDetail/songDetail.js
@@ -8,6 +8,7 @@ Page({
   data: {
     isPlay:false,  //音乐是否在播放
     song:{},  //歌曲详情对象
+    musicId:'', //音乐id
   },
 
   /**
@@ -17,10 +18,38 @@ Page({
     //options:用于接收路由跳转的query参数
     //原生小程序中路由传参，对参数的长度有限制，如果参数长度过长会自动截取掉
     let musicId = options.musicId;
+    this.setData({
+      musicId
+    })
     //获取音乐详情
     this.getMusicInfo(musicId)
 
 
+    // 问题：如果用户操作系统的控制音乐播放、暂停播放的按钮，页面不知道，导致页面显示是否播放的状态和真实的音乐状态不一致
+    // 解决方案：
+    //        1.通过控制音频的实例  BackgroundAudioManager 去监听音乐播放/暂停
+
+    //创建控制音乐播放的实例
+    this.BackgroundAudioManager = wx.getBackgroundAudioManager();
+
+    //监听音乐播放/暂停/停止
+    this.BackgroundAudioManager.onPlay(() => {
+      this.changePlayState(true);
+    });
+    this.BackgroundAudioManager.onPause(() => {
+      this.changePlayState(false);
+    });
+    this.BackgroundAudioManager.onStop(() => {
+      this.changePlayState(false);
+    });
+  },
+
+  //修改播放状态的功能函数
+  changePlayState(isPlay){
+    //修改音乐是否的状态
+    this.setData({
+      isPlay
+    })
   },
 
   //获取音乐详情的功能函数
@@ -40,21 +69,28 @@ Page({
   //点击播放/暂停的回调
   handleMusicPlay(){
     let isPlay = !this.data.isPlay;
-    //修改是否播放状态
-    this.setData({
-      isPlay
-    })
-
-    this.musicControl(isPlay);
+    // //修改是否播放状态
+    // this.setData({
+    //   isPlay
+    // })
+    let {musicId} = this.data
+    this.musicControl(isPlay,musicId);
   },
 
   //控制音乐播放/暂停的功能函数
-  musicControl(isPlay){
+ async musicControl(isPlay,musicId){
+  
     if(isPlay){  //音乐播放
-      //创建控制音乐播放的实例
+      //获取音乐播放链接
+      let musicLinkData = await request('/song/url',{id:musicId})
+      let musicLink = musicLinkData.data[0].url;
       
-    }else{  //暂停音乐播放
-
+      
+      
+      this.BackgroundAudioManager.src = musicLink;
+      this.BackgroundAudioManager.title = this.data.song.name;
+    }else{  //暂停音乐
+      this.BackgroundAudioManager.pause();
     }
   },
 
